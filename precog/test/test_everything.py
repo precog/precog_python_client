07951d8ab21f083c71ead21dfe468ee7323fda4a
Daniel C. Sobral <dcsobral@precog.com>
2013-06-14 11:12:24 -0400

    Do not check warnings, as they include timestamps.

--- precog/test/test_everything.py
+++ precog/test/test_everything.py
@@ -43,7 +43,7 @@ class TestEverything:
         assert response.get('errors') == [], response
         assert response.get('ingested') == 2, response
         print 'append_all_from_string(csv): ok'
-        
+
     def test_json(self):
         # json
         jsondata = {"a": "foo", "b": {"nested": True}, "c": [1,2,3], "d": 4}
@@ -51,33 +51,35 @@ class TestEverything:
         assert response.get('errors') == [], response
         assert response.get('ingested') == 1, response
         print 'append_all: ok'
-        
+
     def test_append(self):
         response = self.api.append("bar", [1,2,3,4])
         assert response.get('ingested') == 1, response
         print 'append: ok'
-        
+
     def test_query1(self):
         response = self.api.query("count(//nonexistent)", "qux")
         assert response == [0], response
         print 'empty count: ok'
-        
+
     def test_query2(self):
         response = self.api.query("count(//nonexistent)", "qux", detailed=True)
-        assert response == {'serverWarnings': [], 'serverErrors': [], 'errors': [], 'data': [0], 'warnings': []}, response
+        assert response['serverErrors'] == [], response
+        assert response['errors'] == [], response
+        assert response['data'] == [0], response
         print 'detailed empty count: ok'
 
     def test_populate1(self):
         self.api.delete("qux/test")
         self.queryUntil("qux", "count(//test)", [0])
         print "delete qux/test: ok"
-        
+
         objs = []
         for i in range(0, 100): objs.append({"i": i, "j": i % 13, "k": "foo"})
         response = self.api.append_all("qux/test", objs)
         assert response['ingested'] == 100, response
         print "populate qux/test: ok"
-        
+
         self.queryUntil("qux", "count(//test)", [100])
         print "count qux/test: ok"
 
@@ -89,7 +91,7 @@ class TestEverything:
         response = self.api.upload_string("qux/test", Format.json, s)
         assert response['ingested'] == 60, response
         print "upload new qux/test: ok"
-        
+
         self.queryUntil("qux", "count(//test)", [60])
         print "count qux/test again: ok"
-        
+
